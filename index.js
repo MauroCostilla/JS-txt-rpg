46460c0607f0bf049ce3c6dfe35463c3a8e126fd
Mauro Costilla <mauroagustincostilla@gmail.com>
2022-10-28 20:08:10 -0300

    primero pasos

---
+++ index.js
@@ -0,0 +1,7 @@
+//Nombre del jugador.
+let nombreJugador = prompt("Hola aventurero, los dioses solicitan su nombre.")
+alert("asi que...  " + nombreJugador)
+alert("los dioses se preguntan si al fin lo encontraron...")
+alert("los dioses solicitan algunos datos para determinar el camino que recorrera su espiritu a lo largo de la travesia.")
+let edad = parseInt(prompt("Empezemos por su edad. (El dios de las mentiras observa sonrriente)(El dios de la verdad observa con intriga)")) 
+let nacimiento = parseInt(prompt("El dia de nacimiento es importante para determinar la fuerza lunar y/o solar sobre su destino. Por favor diganos su dia de nacimiento. "))
