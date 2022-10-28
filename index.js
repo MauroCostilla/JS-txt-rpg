adf549e19d9a84bc61dd15528c2293842c9f531d
Mauro Costilla <mauroagustincostilla@gmail.com>
2022-10-28 20:22:32 -0300

    aaa

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ let nombreJugador = prompt("Hola aventurero, los dioses solicitan su nombre.")
 alert("asi que...  " + nombreJugador)
 alert("los dioses se preguntan si al fin lo encontraron...")
 alert("los dioses solicitan algunos datos para determinar el camino que recorrera su espiritu a lo largo de la travesia.")
-let edad = parseInt(prompt("Empezemos por su edad. (El dios de las mentiras observa sonrriente)(El dios de la verdad observa con intriga)")) 
+let edad = parseInt(prompt("Empezemos por su edad. (El dios de las mentiras observa sonrriente)(El dios de la verdad observa con intriga)"))
 let nacimiento = parseInt(prompt("El dia de nacimiento es importante para determinar la fuerza lunar y/o solar sobre su destino. Por favor diganos su dia de nacimiento. "))
+
+//clase del jugador
+const numero = edad + nacimiento
+
+if (numero > 50){
+    alert("Impresionante. Aventurero, fuiste bendecido por la luna. Tu alma cargada de mana, seguira el camino de la magia")
+} else{
+    alert("interesante. aventurero, su alma fue bendecida por el sol. El sendero del guerrero es el mas adecuado para usted")
+}
