c03238a2907021ebaa59c7fac4f2e75f9c5c0657
Mauro Costilla <mauroagustincostilla@gmail.com>
2022-11-12 23:11:31 -0300

    js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+alert ("Bienvenido jugador!");
+
+const nombre = prompt("primero me gustaria saber tu nombre") 
+
+alert ("Bienvenido " + nombre)
+
+alert ("jugar es muy sencillo, empieza dandole click a la imagen y veras como el contador sube, usa tus puntos para comprar mejoras y pruducir mucho mas. Dicho esto, diviertete " + nombre)
+
+
+
 //inventario
 
 let espiritu = 0;
 
 let inventario = [0,0,0,0,0,0];
-
-console.log(inventario)
 
 let inventarioProduccion = [1,10,15,20,25,30];
 
@@ -19,28 +27,38 @@ let precioProduccion = [50,100,150,200,250,300];
     espiritu++ ;
     };
  
-//funcion para que el evento onclick
+//funcion para  el evento onclick
   
     function comprar(objeto) {
+
+        if(espiritu >= precioProduccion[objeto]){
+            
+            inventario[objeto]++;
+
+            espiritu -= precioProduccion[objeto];
+        }
+        else{
+            alert("no tenes suficiente espiritu")
+        }
  
-        inventario[objeto]++;
+        
     }
 
+//funcion para pruduccion automatica
 
-
-
-
+function producir() {
+    for(contador=0; contador < inventario.length;contador++){
+        
+        espiritu += inventario [contador] * inventarioProduccion[contador];
+    }
+}
 
 //contador
-
-
-
 
 function render(){
     document.getElementById("contador").innerHTML = espiritu;
     document.getElementById("inventario").innerHTML = inventario;
 }
-
 
 
 //-------------------------------------------------
@@ -50,5 +68,6 @@ let FPS = 30;
 setInterval(function(){
 
     render();
+    producir()
 
 },1000/FPS);
