526e90d13d014fa9ee8f2ae58bce31e9422737b3
Mauro Costilla <mauroagustincostilla@gmail.com>
2022-11-12 22:20:28 -0300

    js

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-
+//inventario
 
 let espiritu = 0;
 
 let inventario = [0,0,0,0,0,0];
 
+console.log(inventario)
+
 let inventarioProduccion = [1,10,15,20,25,30];
 
 let precioProduccion = [50,100,150,200,250,300];
 
-function click(){
-    espiritu++;
-};
+//Botones del clicker
+
+
+
+//funcion para que el evento onclick
+
+function comprar(objeto) {
+ 
+    inventario[objeto]++;
+}
+
+
+//contador
+
+clicker.onclick = function(){
+    espiritu++ ;
+}
+
 
 function render(){
     document.getElementById("contador").innerHTML = espiritu;
+    document.getElementById("inventario").innerHTML = inventario;
 }
 
 
