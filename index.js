53cd4ba96e92bd175f736222ff1cf1612e355eba
Mauro Costilla <mauroagustincostilla@gmail.com>
2022-11-18 21:18:58 -0300

    Sin Dom

--- index.js
+++ index.js
@@ -9,23 +9,34 @@ let inventarioProduccion = [1,10,15,20,25,30];
 
 let precioProduccion = [50,100,150,200,250,300];
 
+let ayudantes = [0,0];
 
-//botones de producción
+//ayudantes (objetos)
 
-let prueba1 = document.getElementById("elemento1");
+const orbe = {
+    coste: (8000),
+    produccion: (2),
+
+}
+
+const yokai = {
+    coste: (15000),
+    produccion: (3),
+}
+
+//botones de producción (Dom)
 
 
-prueba1.addEventListener("click",comprar(0))
 
 
 
 //Imagen principal
 
- let clicker = document.getElementById("imagen");
+ //let con dom
 
  clicker.onclick = function(){
     espiritu++ ;
-    }
+    };
  
 //funcion para  el evento onclick
   
@@ -50,14 +61,15 @@ function producir() {
     for(contador=0; contador < inventario.length;contador++){
         
         espiritu += inventario [contador] * inventarioProduccion[contador];
+        espiritu += ayudantes [0] * orbe.produccion
+        espiritu += ayudantes [1] * yokai.produccion
     }
 }
 
 //contador
 
 function render(){
-    document.getElementById("contador").innerHTML = espiritu;
-    document.getElementById("inventario").innerHTML = inventario;
+    //Dom
 }
 
 
